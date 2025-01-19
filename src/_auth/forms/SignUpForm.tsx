308b54079121f79f5fe7f8418998201834f237b9
yelamanq <kulzhanov.elaman@gmail.com>
2025-01-20 00:20:02 +0500

    initial commit

---
+++ src/_auth/forms/SignUpForm.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { SignUpValidation } from "@/lib/validation";
+import { z } from "zod";
+
+const SignUpForm = () => {
+  const form = useForm<z.infer<typeof SignUpValidation>>({
+    resolver: zodResolver(SignUpValidation),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof SignUpValidation>) {
+    // Do something with the form values.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col ">
+        <img src="/assets/images/logo.svg" alt="logo" />
+      </div>
+
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default SignUpForm;
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const SignUpValidation = z.object({
+  name: z.string().min(3, { message: "Name is too short" }),
+  username: z.string().min(3, { message: "Username is too short" }),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" }),
+});
