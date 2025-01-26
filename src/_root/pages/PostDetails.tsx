0fa797aabb88af8c802a372162ba70eedc58825a
yelamanq <kulzhanov.elaman@gmail.com>
2025-01-26 10:46:22 +0500

    feat: delete post && post details added

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,7 +1,106 @@
-import React from "react";
+import Loader from "@/components/shared/Loader";
+import PostStats from "@/components/shared/PostStats";
+import { Button } from "@/components/ui/button";
+import { useUserContext } from "@/context/AuthContext";
+import { useGetPostById } from "@/lib/react-query/queriesAndMutations";
+import { multiFormatDateString } from "@/lib/utils";
+import { useParams, Link } from "react-router-dom";
 
 const PostDetails = () => {
-  return <div>PostDetails</div>;
+  const { id } = useParams();
+  const { data: post, isPending } = useGetPostById(id || "");
+  const { user } = useUserContext();
+
+  const handleDeletePost = () => {};
+
+  return (
+    <div className="post_details-container">
+      {isPending ? (
+        <Loader />
+      ) : (
+        <div className="post_details-card">
+          <img src={post?.imageUrl} alt="post" className="post_details-img" />
+
+          <div className="post_details-info">
+            <div className="flex-between w-full">
+              <Link
+                to={`/profile/${post?.creator.$id}`}
+                className="flex items-center gap-3"
+              >
+                <img
+                  src={
+                    post?.creator?.imageUrl ||
+                    "/assets/icons/profile-placeholder.svg"
+                  }
+                  alt="creator"
+                  className="rounded-full w-8 h-8 lg:h-12 lg:w-12"
+                />
+
+                <div className="flex flex-col">
+                  <p className="base-medium lg:body-bold text-light-1">
+                    {post?.creator.name}
+                  </p>
+                  <div className="flex-center gap-3 text-light-3">
+                    <p className="subtle-semibold lg:small-regular">
+                      {multiFormatDateString(post?.$createdAt)}
+                    </p>
+                    -
+                    <p className="subtle-semibold lg:small-regular">
+                      {post?.location}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+
+              <div className="flex-center gap-2">
+                <Link to={`/update-post/${post?.$id}`}>
+                  <img
+                    src="/assets/icons/edit.svg"
+                    alt="edit"
+                    className={`${user.id !== post?.creator.$id && "hidden"}`}
+                    width={24}
+                    height={24}
+                  />
+                </Link>
+
+                <Button
+                  onClick={handleDeletePost}
+                  variant="ghost"
+                  className={`${
+                    user.id !== post?.creator.$id && "hidden"
+                  } ghost_details-delete_btn`}
+                >
+                  <img
+                    src="/assets/icons/delete.svg"
+                    alt="delete"
+                    width={24}
+                    height={24}
+                  />
+                </Button>
+              </div>
+            </div>
+
+            <hr className="border w-full border-dark-4/80" />
+
+            <div className="flex flex-col flex-1 w-full small-medium lg:base-regular">
+              <p className="">{post?.caption}</p>
+              <ul className="flex gap-1 mt-2">
+                {post?.tags.map((tag: string) => (
+                  <li key={tag} className="text-light-3">
+                    #{tag}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="w-full">
+              <PostStats post={post} userId={user.id} />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default PostDetails;
