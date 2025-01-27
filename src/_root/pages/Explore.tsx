603c3ff98a8cb73f34bf37cef6aa74d142241d8c
yelamanq <kulzhanov.elaman@gmail.com>
2025-01-27 11:01:52 +0500

    api && queries of explore page

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,7 +1,66 @@
-import React from "react";
+import GridPostList from "@/components/shared/GridPostList";
+import SearchResults from "@/components/shared/SearchResults";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 const Explore = () => {
-  return <div>Explore</div>;
+  const [searchValue, setSearchValue] = useState("");
+
+  // const posts = [];
+
+  // const shouldShowSearchResults = searchValue !== "";
+  // const shouldShowPosts =
+  //   !searchValue && posts.pages.every((item) => item.documents.length > 0);
+
+  return (
+    <div className="explore-container">
+      <div className="explore-inner_container">
+        <h2 className="h3-bold md:h2-bold w-full">Search Posts</h2>
+
+        <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
+          <img
+            src="/assets/icons/search.svg"
+            alt="search"
+            width={24}
+            height={24}
+          />
+          <Input
+            className="explore-search"
+            type="text"
+            placeholder="Search"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="flex-between w-full max-w-5xl mt-16 mb-7">
+        <h3 className="body-bold md:h3-bold ">Popular Today</h3>
+
+        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-poiner">
+          <p className="small-medium md:base-medium text-light-2">All</p>
+          <img
+            src="/assets/icons/filter.svg"
+            alt="filter"
+            width={20}
+            height={20}
+          />
+        </div>
+      </div>
+
+      {/* <div className="flex flex-wrap gap-9 w-full max-w-5xl">
+        {shouldShowSearchResults ? (
+          <SearchResults />
+        ) : shouldShowPosts ? (
+          <p className="text-light-4 text-center w-full mt-10">End of posts</p>
+        ) : (
+          posts.pages.map((item, index) => {
+            <GridPostList key={`post-${index}`} post={item.documents} />;
+          })
+        )}
+      </div> */}
+    </div>
+  );
 };
 
 export default Explore;
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -10,10 +10,12 @@ import {
   deletePost,
   deleteSavedPost,
   getCurrentUser,
+  getInfinitePosts,
   getPostById,
   getRecentPosts,
   likePost,
   savePost,
+  searchPosts,
   signInAccout,
   signOutAccount,
   updatePost,
@@ -166,5 +168,27 @@ export const useDeletePost = () => {
         queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
       });
     },
+  });
+};
+
+export const useGetInfinitePosts = () => {
+  return useInfiniteQuery({
+    queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+    queryFn: getInfinitePosts,
+    getNextPageParam: (lastPage) => {
+      if (lastPage && lastPage.documents.length === 0) return null;
+
+      const lastId = lastPage?.documents[lastPage?.documents.length - 1].$id;
+
+      return lastId;
+    },
+  });
+};
+
+export const useSearchPosts = (searchTerm: string) => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.SEARCH_POSTS],
+    queryFn: () => searchPosts(searchTerm),
+    enabled: !!searchTerm,
   });
 };
